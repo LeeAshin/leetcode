4d772c859cad61ed1cd0c4bbe9da87ee44bf7a39
LeeAshin <31952994+LeeAshin@users.noreply.github.com>
2018-04-25 13:45:05 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def test_sql():
-    with open("g_SQL.txt","r") as fp:
-        stat = fp.readline()
-        fp.close()
-    with open (r"D:\WampServer\wamp\www\db-firewall\data\test.txt","a+") as fw:
-        fw.writelines(stat + '\n')
-        fw.close()
